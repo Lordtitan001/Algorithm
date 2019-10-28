909b1c93d596e97cf35986549eb1a0e58a368f17
Kevin <kleumassi1@gmail.com>
2019-10-28 17:36:48 -0400

    debut 3e partie

--- Chemin.java
+++ Chemin.java
@@ -1,3 +1,36 @@
+import java.util.Collection;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
+
 public class Chemin {
+
+    private Map<Integer, Noeud> testedMap =  new TreeMap<Integer, Noeud>();
+    private Map<Integer, Noeud> notTestedMap =  new TreeMap<Integer, Noeud>();
+    private LinkedList<Pair<Integer, LinkedList<Noeud>>> distanceNoeud = new LinkedList<Pair<Integer, LinkedList<Noeud>>>();
+    private boolean commandeOK = true;
+    private Noeud initialNode = new Noeud(0, 0, 0, 0);
     
+    public void initialisation(){
+        Graphe graphe= new Graphe();
+        notTestedMap = graphe.getMapNoeud();
+        graphe.getMapNoeud().values().forEach((node) ->{
+            LinkedList<Noeud> list = new LinkedList<Noeud>();
+            list.add(node);
+            distanceNoeud.add(new Pair<Integer, LinkedList<Noeud>>(Integer.MAX_VALUE, list));
+        });
+    }
+
+    public Noeud sommetMinimal(){
+
+        return initialNode;
+    }
+
+    public void plusCourtChemin(){
+        Graphe graphe= new Graphe();
+        while(!testedMap.containsKey(0) || commandeOK){
+            //graphe.getMapNoeud()
+        }
+    }
 }
--- Noeud.java
+++ Noeud.java
@@ -3,19 +3,6 @@ import java.util.LinkedList;
 
 public class Noeud{
 
-    public class Pair<L,R> {
-        private L l;
-        private R r;
-        public Pair(L l, R r){
-            this.l = l;
-            this.r = r;
-        }
-        public L getL(){ return l; }
-        public R getR(){ return r; }
-        public void setL(L l){ this.l = l; }
-        public void setR(R r){ this.r = r; }
-    }
-
     public Noeud(int numero, int objetA, int objetB, int objetC){
         this.numero = numero;
         this.objetA = objetA;
